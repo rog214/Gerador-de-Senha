1ab66d6e8f4038a0cc95038b3f9b45c122393ce6
Roger <rogerbolad@hotmail.com>
2024-03-06 19:00:42 -0300

    Subindo o Projeto

---
+++ script.js
@@ -0,0 +1,54 @@
+let sliderElement = document.querySelector("#slider");
+let buttonElement = document.querySelector("#button");
+let resetButton = document.querySelector("#reset");
+
+let sizePassword = document.querySelector("#value");
+let password = document.querySelector("#password");
+
+let containerPassword = document.querySelector("#container-password");
+
+let charset = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$&"
+let novaSenha = ""; 
+
+sizePassword.innerHTML = sliderElement.value;
+slider.oninput = function(){
+    sizePassword.innerHTML = this.value;
+}
+
+function generatePassword() {
+    let upperCaseChars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    let lowerCaseChars = "abcdefghijklmnopqrstuvwxyz";
+    let numberChars = "0123456789";
+    let specialChars = "!@#$&";
+    let pass = "";
+    
+    pass += upperCaseChars.charAt(Math.floor(Math.random() * upperCaseChars.length));
+    pass += lowerCaseChars.charAt(Math.floor(Math.random() * lowerCaseChars.length));
+    pass += numberChars.charAt(Math.floor(Math.random() * numberChars.length));
+    pass += specialChars.charAt(Math.floor(Math.random() * specialChars.length));
+
+    let remainingLength = sliderElement.value - 4;
+    let charset = upperCaseChars + lowerCaseChars + numberChars + specialChars;
+    for (let i = 0; i < remainingLength; i++) {
+        pass += charset.charAt(Math.floor(Math.random() * charset.length));
+    }
+
+    pass = pass.split('').sort(() => Math.random() - 0.5).join('');
+
+
+    containerPassword.classList.remove("hide");
+    password.innerHTML = pass;
+    novaSenha = pass;
+}
+
+function copyPassword (){
+    alert("Senha copiada com sucesso!")
+    navigator.clipboard.writeText(novaSenha);
+}
+
+resetButton.addEventListener("click", function() {
+   
+    containerPassword.classList.add("hide");
+    password.innerHTML = "";
+});
+
